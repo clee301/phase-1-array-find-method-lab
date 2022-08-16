6ef4cff51536337ad418952ce34e6566620e60f1
Chan Lee <clee09818@gmail.com>
2022-08-15 23:21:54 -0400

    commit tree a41bf61238645c72f27842a842adf2f81a436207

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array){
+   let something = array.find(element => element.result === 'W')
+    console.log(something)
+    if(!something){
+        return undefined
+    } else {
+        return something.year
+    }
+    
+}
